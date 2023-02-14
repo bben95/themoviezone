641dd95a1cf8e07d5aba837be4226a971d1a0e4e
bben95 <benakabp@gmail.com>
2023-02-14 15:31:46 +0530

    completion row component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,11 +14,11 @@ const Main = () => {
     const movie= movies[Math.floor(Math.random() * movies.length)]
 
   return (
-    <div className='w-full h-screen text-white'>
-        <div className='w-full md:h-[550px] h-2/3 relative'>
+    <div className='w-full h-full text-white '>
+        <div className='w-full h-[550px]  relative'>
             <div className='absolute w-full h-full bg-gradient-to-r from-black'></div>
             <img src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} className="w-full h-full object-cover" />
-            <div className='absolute w-full h-1/2 top-1/2 p-4 md:p-8 flex flex-col gap-4'>
+            <div className='absolute w-full h-1/2 top-1/2 p-4 md:p-8 flex flex-col gap-4 '>
                 <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1>
             <div className='flex gap-4'>
                 <motion.button whileTap={{scale:0.6}} className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>Play</motion.button>
